e08db4f163399c730b627fc433b33f157c94066c
Yazdan <1yazdanrafieemanesh@gmail.com>
2024-08-10 01:24:44 +0330

    a

---
+++ section-11/01-EmailNotificationMicroservice/EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/emailnotification/exception/NotRetryableException.java
@@ -0,0 +1,12 @@
+package com.appsdeveloperblog.emailnotification.exception;
+
+public class NotRetryableException extends RuntimeException {
+
+    public NotRetryableException(String message) {
+        super(message);
+    }
+
+    public NotRetryableException(Throwable cause) {
+        super(cause);
+    }
+}
